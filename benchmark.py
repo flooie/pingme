db5c7f573bc61bcc82d62d4845af1ac5653c9845
William Palin <bill@free.law>
2022-08-05 08:28:13 -1000

    refactor(branches): Drop

--- benchmark.py
+++ benchmark.py
@@ -115,7 +115,7 @@ if __name__ == "__main__":
     print("STARTING UP ---- new branch ...")
     # Benchmark().one_percent_sample()
 
-    benchmark = Benchmark().__init__()
+    benchmark = Benchmark()
     benchmark.root = Path(__file__).parent.absolute()
     print(benchmark.root)
     csv_files = glob.glob(Path.joinpath(benchmark.root, "corpus", f"*.csv").as_posix())
