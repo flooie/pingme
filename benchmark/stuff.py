2b4ff05fca2c615fd3916cecf0252de7b4bfcad8
William Palin <bill@free.law>
2022-08-24 11:34:38 -0400

    feat(add benchmark): Practice

--- benchmark/stuff.py
+++ benchmark/stuff.py
@@ -1,5 +1,11 @@
 """Hello world"""
+import argparse
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--something")
+    args = parser.parse_args()
+
+    print(args.something)
     print("Hello Check Me Branch --- decidedly not main")
     print("And more.")
