86d3a9d48833fa920b52ce2e40a9d190d2711f84
William Palin <bill@free.law>
2022-08-29 15:40:37 -0400

    feat(add benchmark): Practice

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -3,14 +3,11 @@ import argparse
 import bz2
 import csv
 import datetime
+import json
 import os
-import re
 import sys
-from ast import literal_eval as to_list
 from io import StringIO
 from pathlib import Path
-
-import pandas as pd
 from matplotlib import pyplot as plt
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -25,51 +22,18 @@ root = Path(__file__).parent.absolute()
 MAX_ROWS_IN_MD = 51
 
 
+# def get_filepath(self, filename):
+#     return Path.joinpath(root, filename)
+
+
 class Benchmark(object):
     """Benchmark the different eyecite branches"""
 
     def __init__(self):
-        self.now = datetime.datetime.now()
-        self.times = []
-        self.totals = []
-        self.list_of_ids = []
-        self.opinions = []
-        self.count = 0
-        self.fields = []
-        self.gains = []
-        self.losses = []
+        pass
 
     def get_filepath(self, filename):
         return Path.joinpath(root, filename)
-
-    def fetch_citations(self, row) -> None:
-        """Fetch citations from rows opinion data
-
-        return: None
-        """
-        row_id = row[0]
-        row = dict(zip(self.fields, row))
-        # Find opinions to test against from the zip file and return
-        # citations found
-        non_empty_rows = [
-            row[field] for field in self.fields if type(row[field]) == str and len(row[field]) > 0
-        ]
-        if len(non_empty_rows) == 0:
-            return None
-
-        self.list_of_ids.append(row_id)
-        found_cites = []
-        for op in non_empty_rows:
-            if len(op) < 10:
-                continue
-            found_citations = get_citations(clean_text(op, ["html", "inline_whitespace"]))
-            cites = [cite.token.data for cite in found_citations if cite.token]
-            found_cites.extend(cites)
-
-        self.opinions.append(found_cites)
-        self.count += len(found_cites)
-        self.totals.append(self.count)
-        self.times.append((datetime.datetime.now() - self.now).total_seconds())
 
     def generate_branch_report(self, branch: str) -> None:
         """Generate Report
@@ -78,101 +42,92 @@ class Benchmark(object):
         :return: None
         """
         zipfile = bz2.BZ2File(self.get_filepath("bulk-file.csv.bz2"))
-        csv_data = csv.reader(StringIO(zipfile.read().decode()), delimiter=",")
-        self.fields = next(csv_data)
+        csv_data = csv.DictReader(
+            StringIO(zipfile.read().decode()), delimiter=","
+        )
+        count = 0
+        now = datetime.datetime.now()
+        data = []
         for row in csv_data:
-            self.fetch_citations(row)
+            output = {}
+            id = row.pop("id")
+            newrow = {k: v for k, v in row.items() if v}
+            found_citations = get_citations(
+                clean_text(
+                    list(newrow.values())[0], ["html", "inline_whitespace"]
+                )
+            )
+            cites = [cite.token.data for cite in found_citations if cite.token]
+            count += len(cites)
+            output["id"] = id
+            output["cites"] = cites
+            output["total"] = count
+            output["time"] = (datetime.datetime.now() - now).total_seconds()
+            data.append(output)
+        with open(self.get_filepath(f"{branch}.json"), "w") as f:
+            json.dump(data, fp=f, indent=4)
 
-        df = pd.DataFrame(
-            {
-                "OpinionID": self.list_of_ids,
-                "Time": self.times,
-                "Total": self.totals,
-                "Opinions": self.opinions,
-            }
-        )
-        df.to_csv(index=False, path_or_buf=self.get_filepath(f"{branch}.csv"))
-
-    def compare_dataframes(self, main_hash: str, branch_hash: str) -> None:
-        """Compare generated data frames between branches
-
-        Generates (mostly) the markdown report for the PR comment
-
-        Returns: None
-        """
-        main = pd.read_csv(
-            self.get_filepath(f"{main_hash}.csv"),
-            usecols=["OpinionID", "Opinions"],
-        )
-        branch = pd.read_csv(
-            self.get_filepath(f"{branch_hash}.csv"),
-            usecols=["OpinionID", "Opinions"],
-        )
-
-        comparison = main.compare(branch)
-
-        with open(self.get_filepath("output.csv"), "w") as f:
-            writer = csv.writer(f)
-            writer.writerow(["ID", "GAIN", "LOSS", "OPINION_ID", "--"])
-
-            for row in comparison.iterrows():
-                if row[1][0] == row[1][1]:
-                    continue
-                # Convert str list of citations back to list to compare across
-                # branches
-                non_overlap = set(to_list(row[1][0])) ^ set(to_list(row[1][1]))
-                if len(list(non_overlap)) == 0:
-                    continue
-
-                for item in list(non_overlap):
-                    if item in list(row[1][0]):
-                        self.gains.append(item)
-                        row_to_add = [row[0], item, "", main.iat[row[0], 0]]
-                    else:
-                        self.losses.append(item)
-                        row_to_add = [row[0], "", item, branch.iat[row[0], 0]]
-                    writer.writerow(row_to_add)
-
-    def write_report(self, reporters):
+    def write_report(self, reporters, pr_number):
         """Begin building Report.MD file
 
         :return: None
         """
+        max_gain, max_loss = 6, 6
+        gains, losses = 0, 0
+        with open("benchmark/output.csv", mode="r") as inp:
+            reader = csv.DictReader(inp)
+            for row in reader:
+                if row["Gain"]:
+                    gains += 1
+                    max_gain = (
+                        len(row["Gain"])
+                        if len(row["Gain"]) > max_gain
+                        else max_gain
+                    )
+                if row["Loss"]:
+                    losses += 1
+                    max_loss = (
+                        len(row["Loss"])
+                        if len(row["Loss"]) > max_loss
+                        else max_loss
+                    )
+
         with open(self.get_filepath("report.md"), "w") as f:
             f.write("# The Eyecite Report :eye:\n\n")
             f.write("\n\nGains and Losses\n")
             f.write("---------\n")
-            f.write(
-                f"There were {len(self.gains)} gains and "
-                f"{len(self.losses)} losses.\n"
-            )
+            f.write(f"There were {gains} gains and " f"{losses} losses.\n")
             f.write("\n<details>\n")
             f.write("<summary>Click here to see details.</summary>\n\n")
 
-        # Add markdown report file outputs
-        df = pd.read_csv(self.get_filepath("output.csv"))
-
-        with open(self.get_filepath("report.md"), "a") as md:
-            if df.__len__() > MAX_ROWS_IN_MD:
-                with open("report.md", "a+") as f:
-                    f.write(
-                        f"There were {df.__len__()} changes so we are only "
-                        f"displaying the first 50. You can review the \n"
-                        f"entire list by downloading the output.csv "
-                        f"file linked above.\n\n"
-                    )
-
-                df[:MAX_ROWS_IN_MD].to_markdown(buf=md)
-            else:
-                df.to_markdown(buf=md)
-
-        # Remove NAN from file to make it look cleaner
-        with open(self.get_filepath("report.md"), "r+") as f:
-            file = f.read()
-            file = re.sub("nan", "   ", file)
-            f.seek(0)
-            f.write(file)
-            f.truncate()
+            if max_gain + max_loss > MAX_ROWS_IN_MD:
+                f.write(
+                    f"There were {max_gain + max_loss} changes so we are only "
+                    f"displaying the first 50. You can review the \n"
+                    f"entire list by downloading the output.csv "
+                    f"file linked above.\n\n"
+                )
+            row_count = 0
+            with open("benchmark/output.csv", mode="r") as inp:
+                reader = csv.DictReader(inp)
+                header = [
+                    "id".center(10),
+                    "Gain".center(max_gain),
+                    "Loss".center(max_loss),
+                ]
+                f.write(f"| {' | '.join(header)} |\n")
+                blank = ["-" * 10, "-" * max_gain, "-" * max_loss]
+                f.write(f"| {' | '.join(blank)} |\n")
+                for row in reader:
+                    table = []
+                    for value, whitespace_adjust in zip(
+                        list(row.values()), [10, max_gain, max_loss]
+                    ):
+                        table.append(value.center(whitespace_adjust))
+                    f.write(f"| {' | '.join(table)} |\n")
+                    row_count += 1
+                    if row_count > MAX_ROWS_IN_MD:
+                        break
 
         with open(self.get_filepath("report.md"), "a+") as f:
             f.write("\n\n</details>\n")
@@ -183,53 +138,71 @@ class Benchmark(object):
                 f.write("\n\nTime Chart\n")
                 f.write("---------\n")
 
-    def generate_time_chart(self, main, branch, pr_number) -> None:
+        # Add header for time chart for PR comment
+        with open(self.get_filepath("report.md"), "a") as f:
+            f.write(
+                f"![image](https://raw.githubusercontent.com/flooie/pingme/artifacts/benchmark/pr{pr_number}-chart.png)"
+            )
+
+    def generate_time_chart(self, main, branch) -> None:
         """Generate time chart showing speed across branches
 
         return: None
         """
-        main = pd.read_csv(self.get_filepath(f"{main}.csv"))
-        branch = pd.read_csv(self.get_filepath(f"{branch}.csv"))
 
-        main.columns = main.columns.str.replace("Total", "Total Main")
-        branch.columns = branch.columns.str.replace("Total", "Total Branch")
+        with open(f"benchmark/{main}.json", "r") as f:
+            main = json.load(f)
+        with open(f"benchmark/{branch}.json", "r") as b:
+            branch = json.load(b)
 
-        df = pd.merge_asof(main, branch, on="Time")
-        df.plot(kind="line", x="Time", y=["Total Main", "Total Branch"])
+        with open("benchmark/output.csv", "w") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["ID", "Gain", "Loss"])
 
+            for (m, b) in zip(main, branch):
+                if set(m["cites"]) == set(b["cites"]):
+                    continue
+
+                changes = set(m["cites"]) ^ set(b["cites"])
+                for change in list(changes):
+                    gain, loss = "", ""
+                    if change in m["cites"]:
+                        loss = change
+                    else:
+                        gain = change
+                    writer.writerow([m["id"], gain, loss])
+
+        plt.plot(
+            [x["time"] for x in main],
+            [x["total"] for x in main],
+            label=main,
+        )
+        plt.plot(
+            [x["time"] for x in branch],
+            [x["total"] for x in branch],
+            label=branch,
+        )
+        plt.legend()
         plt.ylabel("# Cites Found ", rotation="vertical")
+        plt.xlabel("Seconds")
+        plt.title("Comparison of Branches")
         plt.savefig(self.get_filepath("chart.png"))
-
-        # Add header for time chart for PR comment
-        with open(self.get_filepath("report.md"), "a") as f:
-            f.write(f"![image](https://raw.githubusercontent.com/flooie/pingme/artifacts/benchmark/pr{pr_number}-chart.png)")
 
 
 if __name__ == "__main__":
+    """"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--chart")
-    parser.add_argument("--main")
-    parser.add_argument("--branch")
+    parser.add_argument("--pr")
+    parser.add_argument("--branches", nargs='+')
     parser.add_argument("--reporters", action="store_true")
 
     args = parser.parse_args()
-    branch = args.branch
-    main = args.main
-    chart = args.chart
-    reporters = args.reporters
+    # reporters = args.reporters
 
     benchmark = Benchmark()
-    if args.chart:
-        # Process the report
-        benchmark.compare_dataframes(main, branch)
-
-        # Write Report.MD file
-        benchmark.write_report(reporters)
-
-        if not reporters:
-            # Generate time chart
-            benchmark.generate_time_chart(main, branch, chart)
-    else:
-        # Generate data comparison
-        version = main if main else branch
-        benchmark.generate_branch_report(branch=version)
+    if len(args.branches) == 1:
+        benchmark.generate_branch_report(branch=args.branches[0])
+    elif len(args.branches) == 2:
+        benchmark.generate_branch_report(branch=args.branches[1])
+        benchmark.generate_time_chart(args.branches[0], args.branches[1])
+        benchmark.write_report(reporters=False, pr_number=args.pr)
